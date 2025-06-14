53dc22a8079bedb95768f2585ec20e10b325bfa4
FeS <FeS@DESKTOP-4J43KGR>
2025-06-14 18:08:20 -0500

    Agregar archivos de proyecto.

---
+++ VisualizadorDeFotos/Carpeta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VisualizadorDeFotos
+{
+    internal class Carpeta
+    {
+        public string Nombre { get; set; }
+        public string Ruta { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+
+        public Carpeta(string ruta)
+        {
+            
+        }
+
+        private void CargarImagenes()
+        {
+           
+        }
+
+        public List<Imagen> ListarImagenes() => Imagenes;
+    }
+}
---
+++ VisualizadorDeFotos/Imagen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VisualizadorDeFotos
+{
+    internal class Imagen
+    {
+        public string Ruta { get; set; }
+        public string Nombre { get; set; }
+        public string Formato { get; set; }
+        public long Tamaño { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int Rotacion { get; set; }
+
+        public Imagen(string ruta)
+        {
+           
+        }
+
+        public Image Mostrar()
+        {
+            
+        }
+
+        public void Rotar(int grados)
+        {
+            
+        }
+
+        public string ObtenerInfo()
+        {
+            
+        }
+    }
+}
